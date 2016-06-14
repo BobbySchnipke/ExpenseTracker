e297582499d951deeae7ae2ebfb8bb7dc48ce634
BobbySchnipke <bobbyschnipke@gmail.com>
2016-06-14 18:19:08 -0400

    UI complete on Add Expenses Form and Home Form.

--- ExpenseTracker/Report.Designer.cs
+++ ExpenseTracker/Report.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ExpenseTracker
 {
-    partial class Form1
+    partial class Report
     {
         /// <summary>
         /// Required designer variable.
@@ -30,10 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "Report";
         }
 
         #endregion
     }
 }
-
